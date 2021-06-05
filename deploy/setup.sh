4514fd25f8801a657e83db47eb9f791341cf4325
陳博煒 <rayologist1002@gmail.com>
2021-06-06 04:16:46 +0800

    deploy bug fixed

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -12,11 +12,11 @@ apt-get update
 apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
 # Create project directory
-[ ! -d "$PROJECT_BASE_PATH" ] && mkdir -p $PROJECT_BASE_PATH
-[ -d "$PROJECT_BASE_PATH" ] || git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+mkdir -p $PROJECT_BASE_PATH
+git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 # Create virtual environment
-[ ! -d "$PROJECT_BASE_PATH/env" ] && mkdir -p $PROJECT_BASE_PATH/env
+mkdir -p $PROJECT_BASE_PATH/env
 python3 -m venv $PROJECT_BASE_PATH/env
 
 # Install python packages
@@ -36,7 +36,7 @@ supervisorctl restart profiles_api
 
 # Configure nginx
 cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
-[ -d "/etc/nginx/sites-enabled/default" ] || rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
 systemctl restart nginx.service
 
